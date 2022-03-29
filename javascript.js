1bfde02e2eb52338ed23c75df995f341df530931
Spec0s <edgaras2112@mail.ru>
2022-03-29 18:44:31 +0300

    Added first function computerPlay

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
 console.log("Hello there!")
+const gameSymbols=["Rock","Paper","Scissors"]
+ function computerPlay(){
+     return gameSymbols[Math.floor(Math.random()*gameSymbols.length)]
+ }
+ console.log(computerPlay());
